ee25032bb679dce05ab35bf48c927dfaf53d5881
JordyMorenoArias <yordimorenoarias.11@gmail.com>
2025-01-07 17:50:35 -0400

    Implementación del diseño del formulario de creación de tareas
    - Se crearon los archivos HTML y CSS para el componente "div-create-task".
    - Diseño del formulario de creación de tareas con estilos modernos y responsivos.
    - Estructura HTML que incluye encabezado, cuerpo y pie de formulario.
    - Estilos CSS aplicados para:
      - Fondo semitransparente del contenedor principal.
      - Formulario centrado con bordes redondeados y sombra.
      - Entradas, área de texto, selectores y botón con diseño consistente.
    - Preparado para futuras interacciones con JavaScript.

--- app.py
+++ app.py
@@ -2,15 +2,42 @@ from flask import Flask, render_template, request, redirect, session, url_for
 app = Flask(__name__)
 app.secret_key = b'\x97\xa6\xe3?Bm\xdcL!I=\x08\xe96\x18\xd6q\x9bq\x83\xce8\xbd&'
 import pymysql
+import pymysql.cursors
 import hashlib
 
 def InsertarUsuario(user, password):
-    connection = pymysql.connect(host="localhost", user="root", password="", database="loginpython", port=3306,)
+    connection = pymysql.connect(
+        host="localhost", 
+        user="root", 
+        password="", 
+        database="gestortareas", 
+        port=3306,
+    )
+    
     cursor = connection.cursor()
     cursor.execute("INSERT INTO Users (User, password) VALUES (%s, %s)", (user, HashearPassword(password)))
     connection.commit()
     cursor.close()
     connection.close()
+
+def obtenertareas(IdUser): 
+    connection = pymysql.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="gestortareas",
+        port=3306,
+        cursorclass=pymysql.cursors.DictCursor
+    )
+
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM Tareas WHERE IdUser = %s", (IdUser,))
+            tareas = cursor.fetchall()
+        return tareas
+    finally:
+        connection.close()
+
     
 def HashearPassword(password):
     return hashlib.sha256(password.encode('utf-8')).hexdigest()
@@ -45,4 +72,15 @@ def home():
     else:
         return redirect(url_for('Login'))
     
-
+@app.route('/dashboard/tareas', methods=['POST'])
+def crearTarea():
+    if 'IdUser' in session:
+        IdUser = session['IdUser']
+        tarea = request.form['tarea']
+        
+        connection = pymysql.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="gestortareas",
+        port=3306)
