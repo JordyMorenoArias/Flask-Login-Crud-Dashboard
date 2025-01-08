3a97936085ab64081695adc4bbb0fe964faa81dc
JordyMorenoArias <yordimorenoarias.11@gmail.com>
2025-01-08 11:54:46 -0400

    Implementación de funcionalidad para creación de tareas:
    - Añadido código JavaScript para gestionar la captura de datos del formulario y enviar las solicitudes al servidor.
    - Implementado endpoint en Python para recibir los datos, procesarlos y almacenarlos en la base de datos MySQL.
    - Asegurada la validación de datos tanto en el cliente como en el servidor.
    - Incluye manejo de errores en el cliente y en el servidor para garantizar una experiencia robusta.

--- app.py
+++ app.py
@@ -1,46 +1,60 @@
-from flask import Flask, render_template, request, redirect, session, url_for
-app = Flask(__name__)
-app.secret_key = b'\x97\xa6\xe3?Bm\xdcL!I=\x08\xe96\x18\xd6q\x9bq\x83\xce8\xbd&'
+from datetime import date
+from flask import Flask, jsonify, render_template, request, redirect, session, url_for, session
 import pymysql
 import pymysql.cursors
 import hashlib
 
-def InsertarUsuario(user, password):
-    connection = pymysql.connect(
-        host="localhost", 
-        user="root", 
-        password="", 
-        database="gestortareas", 
-        port=3306,
-    )
-    
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO Users (User, password) VALUES (%s, %s)", (user, HashearPassword(password)))
-    connection.commit()
-    cursor.close()
-    connection.close()
+app = Flask(__name__)
+app.secret_key = b'\x97\xa6\xe3?Bm\xdcL!I=\x08\xe96\x18\xd6q\x9bq\x83\xce8\xbd&'
 
-def obtenertareas(IdUser): 
+def HashearPassword(password):
+    return hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+def InsertarUsuario():
     connection = pymysql.connect(
         host="localhost",
         user="root",
         password="",
-        database="gestortareas",
+        database="taskmanager",
+        port=3306,
+    )
+    
+    cursor = connection.cursor()
+    
+    cursor.execute(
+        """
+        INSERT INTO Users (name, email, password_hash, create_date)
+        VALUES (%s, %s, %s, %s)
+        """,
+        (
+            "Jordy Moreno Arias",
+            "yordimorenoarias.11@gmail.com",
+            HashearPassword("2001892z"),
+            date.today()
+        )
+    )
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+def obtenertareas(UserId): 
+    connection = pymysql.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="taskmanager",
         port=3306,
         cursorclass=pymysql.cursors.DictCursor
     )
 
     try:
         with connection.cursor() as cursor:
-            cursor.execute("SELECT * FROM Tareas WHERE IdUser = %s", (IdUser,))
+            cursor.execute("SELECT * FROM Tasks WHERE user_Id = %s", (UserId,))
             tareas = cursor.fetchall()
         return tareas
     finally:
         connection.close()
-
-    
-def HashearPassword(password):
-    return hashlib.sha256(password.encode('utf-8')).hexdigest()
 
 @app.route('/', methods=['GET', 'POST'])
 def Login():
@@ -51,36 +65,85 @@ def Login():
         if not email or not password:
             return render_template('login.html', error="Los campos no pueden estar vacíos")
         
-        with pymysql.connect(host="localhost", user="root", password="", database="loginpython", port=3306) as connection:
+        with pymysql.connect(host="localhost", user="root", password="", database="taskmanager", port=3306) as connection:
             with connection.cursor() as cursor:
-                cursor.execute("SELECT Id FROM Users WHERE User = %s AND Password = %s", (email, HashearPassword(password)))
+                cursor.execute("SELECT user_Id FROM users WHERE email = %s AND password_hash = %s;", (email, HashearPassword(password)))
                 result = cursor.fetchone()
-        
         if result:
             session['IdUser'] = result[0]
-            return redirect(url_for('home'))    
+            return redirect(url_for('dashboard'))    
         else:
             return render_template('login.html', error="Usuario o contraseña incorrectos")
     
     return render_template('login.html')
 
 @app.route('/dashboard')
-def home():
+def dashboard():
     if 'IdUser' in session:
-        IdUser = session['IdUser']
-        return render_template('dashboard.html', IdUser=IdUser)
+        UserId = session['IdUser']
+        return render_template('dashboard.html', UserId=UserId)
     else:
         return redirect(url_for('Login'))
     
-@app.route('/dashboard/tareas', methods=['POST'])
-def crearTarea():
-    if 'IdUser' in session:
-        IdUser = session['IdUser']
-        tarea = request.form['tarea']
+@app.route('/task', methods=['POST'])
+
+def createTask():
+    if 'IdUser' not in session:
+        return jsonify({'error': 'Usuario no autenticado'}), 401
+    
+    try:
+        datos = request.get_json()
+        if not datos:
+            return jsonify({'error': 'No se recibieron datos'}), 400
         
-        connection = pymysql.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="gestortareas",
-        port=3306)
+        UserId = session['IdUser']
+
+        try:
+            connection = pymysql.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="taskmanager",
+            port=3306
+            )
+            print("Conexión exitosa")           
+        except Exception as e:
+            print("Error en la conexión:", e)
+
+
+        try:
+            with connection.cursor() as cursor:
+                query = "INSERT INTO Tasks (user_Id, task_name, description, priority, category, expiration_date, status) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+                print(UserId)
+                print(datos)
+                cursor.execute(query, (
+                    UserId, 
+                    datos.get('titulo'), 
+                    datos.get('descripcion'), 
+                    datos.get('prioridad'), 
+                    datos.get('categoria'), 
+                    datos.get('fecha'), 
+                    'En Proceso'
+                ))
+                
+                connection.commit()
+                return jsonify({
+                    'mensaje': 'Tarea creada exitosamente',
+                    'id': cursor.lastrowid
+                }), 201
+
+        except Exception as e:
+            connection.rollback()
+            return jsonify({
+                'error': 'Error al crear la tarea',
+                'mensaje': str(e)
+            }), 500
+            
+        finally:
+            connection.close()
+            
+    except Exception as e:
+        return jsonify({
+            'error': 'Error en el servidor',
+            'mensaje': str(e)
+        }), 500
--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,9 +1,9 @@
-function ShowHideCreateTask(){
+function ShowHideCreateTask() {
     var container = document.getElementsByClassName("create-Task-Container")[0];
 
-    if(container.style.visibility === "hidden" || container.style.visibility === ""){
+    if (container.style.visibility === "hidden" || container.style.visibility === "") {
         container.style.visibility = "visible";
-    }else{
+    } else {
         container.style.visibility = "hidden";
     }
 }
@@ -17,4 +17,46 @@ function HideOnBackgroundClick(event) {
     if (!taskForm.contains(event.target)) {
         container.style.visibility = "hidden";
     }
+}
+
+document.querySelector(".save-task").addEventListener("click", CreateTask);
+function CreateTask() {
+    const dataTask = {
+        titulo: document.querySelector(".txt-task-title").value,
+        categoria: document.querySelector(".txt-task-category").value,
+        descripcion: document.querySelector(".txt-task-description").value,
+        prioridad: document.querySelector(".txt-task-priority").value,   
+        fecha: document.querySelector(".txt-task-date").value,   
+    }
+
+    fetch('/task',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dataTask)
+    })
+    .then(respuesta => {
+        if (!respuesta.ok) {
+            throw new Error('La respuesta de la red no fue correcta');
+        }
+        return respuesta.json();
+    })
+    .then(datos => {
+        console.log('Éxito:', datos);
+        limpiarFormulario();
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+ }
+
+ function limpiarFormulario() {
+    document.querySelector('.txt-task-title').value = '';
+    document.querySelector('.txt-task-category').value = 'personal';
+    document.querySelector('.txt-task-description').value = '';
+    document.querySelector('.txt-task-priority').value = '';
+    document.querySelector('.txt-task-date').value = 'low';
+    
+    document.querySelector('.create-Task-Container').style.visibility = 'hidden';
 }
